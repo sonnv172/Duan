f52c676ae5d8c195fc33167a6f1930ad1a931d61
sonnv172 <sononvph07865@fpt.edu.vn>
2019-11-18 14:04:37 +0700

    Update xml

---
+++ app/src/main/java/com/example/duan_1/nguoichoi.java
@@ -0,0 +1,14 @@
+package com.example.duan_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nguoichoi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nguoichoi);
+    }
+}
